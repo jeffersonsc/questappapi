b1f58485f04363e363ebf25bc58efcf501c1aad0
Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>
2016-04-06 10:54:40 -0300

    Set ajusting of routes create api route
    Signed-off-by: Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   
+  #Api definition using namespace set #api in controller
+  #Format default of response :json
+  #Set constraint of subdomain to api exemple api.example.com
+  #Set path to root directory
+  namespace :api, defaults: {format: :json}, constraints: {subdmain: :api}, path: '/' do
+    #Set all routes types :get, :post, :put, :delete
+  end
+
 end
