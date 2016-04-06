8e1f53f51550eef049fa57da18d2f04cfeeaaff0
Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>
2016-04-06 10:59:39 -0300

    Ajusting in routes add version of api
    Signed-off-by: Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   #Set path to root directory
   namespace :api, defaults: {format: :json}, constraints: {subdmain: :api}, path: '/' do
     #Set all routes types :get, :post, :put, :delete
+    #Set version of Api /v1/contents
+    scope module: :v1 do
+      #Set routes in v1 api in folder #controllers/api/v1
+    end
   end
 
 end
