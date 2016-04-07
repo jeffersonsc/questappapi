f3bdea8254d8ef7b5442aa3e6e6f671e34281cd1
Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>
2016-04-07 15:51:56 -0300

    Ajust test valid not save nulls
    Signed-off-by: Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>

--- spec/models/landing_page_spec.rb
+++ spec/models/landing_page_spec.rb
@@ -3,14 +3,18 @@ require 'rails_helper'
 
 RSpec.describe "LandingPage", type: :model do
   #Generate factory girl
-  before {@user = FactoryGirl.build(:landing_page)}
+	before {@user = FactoryGirl.build(:landing_page)}
 
   #Set user in context
   subject {@user}
-
   #Valid email not empty in landing page
   it {should respond_to(:email)}
-
   it {should be_valid}
+
+
+  describe "Email is not valid empty" do
+  	before { @user.email = " " }
+  	it {should_not be_valid}
+  end
 
 end
