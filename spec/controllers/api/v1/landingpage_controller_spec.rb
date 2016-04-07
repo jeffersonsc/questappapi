5ec66a37d1c53f06dd6c7586fcb47709b581b3b3
Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>
2016-04-07 18:00:59 -0300

    Create test of landing page create
    Signed-off-by: Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>

--- spec/controllers/api/v1/landingpage_controller_spec.rb
+++ spec/controllers/api/v1/landingpage_controller_spec.rb
@@ -14,6 +14,40 @@ RSpec.describe Api::V1::LandingpageController, type: :controller do
 			expect(user_response[:email]).to eql @user.email
 		end
 
-		it { expect(response).to be_success }
+		it { expect(response).to have_http_status(200) }
+	end
+
+	describe "POST #create" do
+		context "User has be created"
+			before(:each) do
+				@user_attributes = FactoryGirl.attributes_for :landing_page
+				post :create, {landing_page: @user_attributes}, format: :json
+			end
+
+			it "Render JSON after created successful" do
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				expect(user_response[:email]).to eql @user_attributes[:email]
+			end
+
+			it {expect(response).to have_http_status(201)}
+		end
+
+		context "User not creted test when" do
+			before(:each) do
+				#create parmas without email
+				@user_invalid_attributes = {}
+				post :create, {landing_page: @user_invalid_attributes}, format: :json
+			end
+
+			it "Render json error" do
+				user_response = JSON.parse(response.body, symbolize_names: true)
+				expect(user_response).to have_key(:errors)
+			end
+
+			it "Return error message to email" do
+
+			end
+		end
+
 	end
 end
