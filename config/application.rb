49517a1524f9248ab949a830c5abcf6c4243cfb2
Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>
2016-04-11 11:18:19 -0300

    Setup application rack of test rspec and routes application
    Signed-off-by: Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module QuestappApi
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -22,5 +22,17 @@ module QuestappApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+
   end
 end
