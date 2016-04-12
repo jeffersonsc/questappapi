eb7e0ef638615882511eb25431d8dac0800ba440
Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>
2016-04-12 15:22:48 -0300

    User create auth_tohen to api
    Signed-off-by: Jefferson Da Silva Cunha <jeffersondasilva@boldint.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :generate_authentication_token!
+
   validates :auth_token, uniqueness: true
+
+
+  #Generate token for authetication user
+  def generate_authentication_token!
+  	begin
+  		self.auth_token = Devise.friendly_token
+  	end while self.class.exists?(auth_token: auth_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,20 @@ RSpec.describe User, type: :model do
 	it{should validate_uniqueness_of(:email).case_insensitive}
 	it{should validate_confirmation_of(:password)}
 	it{should allow_value("exemple@domain.com").for(:email)}
-	#it{should validate_uniqueness_of(:auth_token)}
+	it{should validate_uniqueness_of(:auth_token)}
+
+	describe "#Generate authentication token!" do
+		it "Generate a unique toekn" do
+			Devise.stub(:friendly_token).and_return("auniquetoken123")
+			@user.generate_authentication_token!
+			expect(@user.auth_token).to eql "auniquetoken123"
+		end
+
+		it "Generate another token when one already has been token" do
+			existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
+			@user.generate_authentication_token!
+			expect(@user.auth_token).not_to eql existing_user.auth_token
+		end
+	end
 
 end
